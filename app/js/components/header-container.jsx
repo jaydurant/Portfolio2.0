66511d6a9609e3b1b736042109f5995a0fab1822
durantjay <durantjay1@gmail.com>
2016-04-01 16:48:18 -0700

    Add keyboard icon to header

--- app/js/components/header-container.jsx
+++ app/js/components/header-container.jsx
@@ -3,10 +3,11 @@ import MenuContainer from './menu-container';
 import { IndexLink } from 'react-router';
 
 function HeaderContainer(props) {
+	const spaceIcon = <i className="fa fa-keyboard-o fa-lg"></i>;
 	return (
 		<header className="container header-container">
 			<div className="one-half-m menu-home-link">
-				<IndexLink to="/">JASON DURANT - FRONT END DEVELOPER</IndexLink>
+				<IndexLink to="/">JASON DURANT {spaceIcon} FRONT END DEVELOPER</IndexLink>
 			</div>
 			<nav className="one-half-m">
 				<MenuContainer {...props} />
