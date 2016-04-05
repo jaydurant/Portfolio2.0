54986623f3bd8be000231d0d1cd76d3a2de9f0cf
durantjay <durantjay1@gmail.com>
2016-04-05 12:23:52 -0700

    Add project item github and projects demo link

--- app/js/components/project-item.jsx
+++ app/js/components/project-item.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function ProjectsItem(props) {
+	console.log(props);
+	const tools = props.tools.map((val, i) => <li key={i}>{val}</li>);
 	return (
 		<div className="one-half-m project-item-container">
 			<div className="project-item project-item-flipper">
@@ -12,6 +14,16 @@ function ProjectsItem(props) {
 				</div>
 				<div className="project-item-description project-item-back">
 					<p>{props.description}</p>
+					<div>
+						<h3>Tools & Links</h3>
+						<ul className="one-half-s">
+							{tools}
+						</ul>
+						<div className="one-half-s project-links">
+							<a href={props.github}>Github Page</a>
+							<a href={props.demo}>Project Demo</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -22,6 +34,9 @@ ProjectsItem.propTypes = {
 	name: React.PropTypes.string,
 	description: React.PropTypes.string,
 	image: React.PropTypes.string,
+	tools: React.PropTypes.array,
+	github: React.PropTypes.string,
+	demo: React.PropTypes.string,
 };
 
 export default ProjectsItem;
