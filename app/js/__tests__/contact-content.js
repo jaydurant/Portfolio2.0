ea4ea08b445b3a38d1cf6cc1ef57223ec84e97d1
durantjay <durantjay1@gmail.com>
2016-04-14 12:39:01 -0700

    Add contact-content unit test

--- app/js/__tests__/contact-content.js
+++ app/js/__tests__/contact-content.js
@@ -1,8 +1,8 @@
-jest.unmock('../components/about-content');
+jest.unmock('../components/contact-content');
 
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
-import ContactContent from '../components/about-content';
+import ContactContent from '../components/contact-content';
 
 describe('ContactContent', ()=> {
 	const renderer = TestUtils.createRenderer();
@@ -10,9 +10,11 @@ describe('ContactContent', ()=> {
 	const renderedTree = renderer.getRenderOutput();
 	const h1Element = renderedTree.props.children[0];
 	const contactUrl = renderedTree.props.children[1].props.children[1];  
+	const buttonList = renderedTree.props.children[2].props.children[1].props.children;
 
 	it("displays author's content", () => {
-		console.log(h1Element)
-		expect(h1Element.props.children).toMatch(/about/i);
+		expect(h1Element.props.children).toMatch(/contact/i);
+		expect(contactUrl.props.href).toMatch(/durantjay/i);
+		expect(buttonList.length).toBe(4);
 	})
 })
